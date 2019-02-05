1da379ae5e0e2d57003e2441171c230ba78256ab
oomusou <oomusou@gmail.com>
2019-02-05 15:45:10 +0800

    改用 JSON

--- node/index.js
+++ node/index.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 
-const todos = require('./todos');
+const data = require('./todos.json');
 
 const app = express().use(cors());
 const hostname = '127.0.0.1';
 const port = 3000;
 
-app.get('/todos', todos);
+app.get('/todos', (req, res) => res.send(data));
 
-app.listen(port, () => {
-  console.log(`Serving running at http://${hostname}:${port}/`);
-});
+app.listen(port, () =>
+  console.log(`Serving running at http://${hostname}:${port}/`));
